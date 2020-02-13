4fc3143a2eb8e39eff2b98777b6d192251921093
Maksim_Dolnik <maksim.dolnik@wrenkitchens.com>
2020-02-13 13:34:36 +0300

    Slider snapping turned off (train movement fix)

--- Assets/Scripts/Controllers/CameraControllerScript.cs
+++ Assets/Scripts/Controllers/CameraControllerScript.cs
@@ -1,50 +1,42 @@
 ﻿using UnityEngine;
 
-public class CameraControllerScript : MonoBehaviour
+namespace Controllers
 {
-    public Transform target;
-    [SerializeField] private float _smoothStep = 0.125f;
-    private Vector3 offset;         
-    [SerializeField] private RectTransform _storeButton;
-    [SerializeField] private Transform _initialRail;
-    private Camera _camera;
+    public class CameraControllerScript : MonoBehaviour
+    {
+        public Transform target;
+        [SerializeField] private float _smoothStep = 0.125f;
+        private Vector3 offset;         
+        [SerializeField] private RectTransform _storeButton;
+        [SerializeField] private Transform _initialRail;
+        private Camera _camera;
 
-    private float H;
-    private float S;
-    private float V;
-    
-    // Use this for initialization
-    void Start()
-    {
-        _camera = GetComponent<Camera>();
-        var storeButtonPosition = _camera.ScreenToWorldPoint(_storeButton.position) + Vector3.up*0.15f;
-        
-        storeButtonPosition.z = 0;
-        target.position = storeButtonPosition;
-        _initialRail.position = storeButtonPosition;
-        
-        offset = transform.position - target.position;
-        
-        Color.RGBToHSV(_camera.backgroundColor,out H,out S,out V);
-        H *= 360;
-    }
-    
-    void FixedUpdate()
-    {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothStep);
-        transform.position = smoothedPosition;
-    }
+        [SerializeField] private Gradient _gradient;
 
-    void LateUpdate()
-    {
-        if (H >= 360)
+        [SerializeField] private float _gradientSmoothness;
+        // Use this for initialization
+        void Start()
         {
-            H = 0;
+            _camera = GetComponent<Camera>();
+            var storeButtonPosition = _camera.ScreenToWorldPoint(_storeButton.position) + Vector3.up*0.15f;
+        
+            storeButtonPosition.z = 0;
+            target.position = storeButtonPosition;
+            _initialRail.position = storeButtonPosition;
+        
+            offset = transform.position - target.position;
         }
-        
-        H += Time.deltaTime*2;
-        
-        _camera.backgroundColor = Color.HSVToRGB(H / 360, S, V);
+    
+        void FixedUpdate()
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothStep);
+            transform.position = smoothedPosition;
+        }
+    
+        void Update()
+        {
+            _camera.backgroundColor = _gradient.Evaluate(Mathf.PingPong(Time.time/_gradientSmoothness, 1));
+        }
     }
 }
